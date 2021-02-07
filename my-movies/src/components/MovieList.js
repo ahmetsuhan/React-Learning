c0a6054861ac6189dc06a73957128d274579ef25
Ahmet Suhan Oka <ahmetsuhanoka0@gmail.com>
2021-02-08 01:38:58 +0300

    MovieList changed stateless func comp

--- my-movies/src/components/MovieList.js
+++ my-movies/src/components/MovieList.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-class MovieList extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="row">
-          <div className="col-lg-4">
+const MovieList = (props) => {
+  return (
+    <div>
+      <div className="row">
+        {props.movies.map((movie) => (
+          <div className="col-lg-4" key={movie.id}>
             <div className="card mb-4 shadow-sm">
               <img
-                src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg"
+                src={movie.imageURL}
                 className="card-img-top"
                 alt="sample movie"
               ></img>
               <div className="card-body">
-                <h5 className="card-title">Sample Movie</h5>
-                <p className="card-text">Sample Movie description</p>
+                <h5 className="card-title">{movie.name}</h5>
+                <p className="card-text">{movie.overview}</p>
                 <div className="d-flex justify-content-between align-items-center">
                   <button
                     type="button"
@@ -23,16 +23,16 @@ class MovieList extends React.Component {
                     Delete
                   </button>
                   <h2>
-                    <span className="badge bg-info">9.0</span>{" "}
+                    <span className="badge bg-info">{movie.rating}</span>
                   </h2>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default MovieList;
