7c724843687bc800984c40813cb0a925c3feb459
Ahmet Suhan Oka <ahmetsuhanoka0@gmail.com>
2021-02-07 20:16:06 +0300

    func component turned into class component

--- react-begin/src/Components/Card.js
+++ react-begin/src/Components/Card.js
@@ -1,22 +1,20 @@
-import React from 'react'
+import React, { Component } from "react";
 
-
-const Card = (props) => {
-//console.log(props);
+class Card extends Component {
+  render() {
     return (
-            <div className="card w-100">
-          <img src={props.image} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{props.cardTitle}</h5>
-            <p className="card-text">
-              {props.cardText}
-            </p>
-          </div>
-          <div className="card-pfooter">
-            <small className="text-muted">{props.updatedTime}</small>
-          </div>
+      <div className="card w-100">
+        <img src={this.props.image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{this.props.cardTitle}</h5>
+          <p className="card-text">{this.props.cardText}</p>
         </div>
-    )
+        <div className="card-pfooter">
+          <small className="text-muted">{this.props.updatedTime}</small>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Card;
