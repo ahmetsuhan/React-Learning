9f658f156bf3a828f86784a85d36ed2cacd4f25c
Ahmet Suhan Oka <ahmetsuhanoka0@gmail.com>
2021-02-07 21:44:36 +0300

    Collapse.Js turned into a Class Component

--- react-begin/src/Components/Collapse.js
+++ react-begin/src/Components/Collapse.js
@@ -1,29 +1,31 @@
 import React from "react";
 
-const Collapse = (props) => {
+class Collapse extends React.Component {
 //console.log(props.children);
 
-
-  return (
-    <div>
-      <p>
-        <a
-          className="btn btn-primary w-100"
-          data-bs-toggle="collapse"
-          href={"#".concat(props.href)}
-          role="button"
-          aria-expanded="false"
-          aria-controls="collapseExample"
-        >
-          Link with href
-        </a>
-       
-      </p>
-      <div className="collapse show" id={props.href}>
-        {props.children}
-      </div>
-    </div>
-  );
+render(){
+    return (
+        <div>
+          <p>
+            <a
+              className="btn btn-primary w-100"
+              data-bs-toggle="collapse"
+              href={"#".concat(this.props.href)}
+              role="button"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >
+              Link with href
+            </a>
+           
+          </p>
+          <div className="collapse show" id={this.props.href}>
+            {this.props.children}
+          </div>
+        </div>
+      );
+}
+ 
 };
 
 export default Collapse;
