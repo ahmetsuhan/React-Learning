cbb8167d3b1f788e3da9a32aa848259dea0ac741
Ahmet Suhan Oka <ahmetsuhanoka0@gmail.com>
2021-02-09 19:10:46 +0300

    router added

--- my-movies/src/App.js
+++ my-movies/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import SearchBar from "./components/Searchbar";
 import MovieList from "./components/MovieList";
 import axios from "axios";
-import AddMovie from './components/AddMovie';
-require('dotenv').config();
+import AddMovie from "./components/AddMovie";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+//require('dotenv').config();
 
 console.log(process.env.REACT_APP_API_KEY);
 
@@ -23,25 +25,23 @@ class App extends React.Component {
       this.setState({movies:data});
     }
    */
-  
+
   //axios: http istekleri yapmak için kullanılan promise tabanlı bir library
   async componentDidMount() {
     const baseURL = "http://localhost:3002/movies";
-    const baseURL2 =`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
-     ;
-     const baseURL3 = `https://api.themoviedb.org/3/list/7076296?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
+    const baseURL2 = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`;
+    const baseURL3 = `https://api.themoviedb.org/3/list/7076296?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
 
-      const response = await axios.get(baseURL);
-      console.log(response);
-      this.setState({movies:response.data});
-     
-/*
+    const response = await axios.get(baseURL);
+    console.log(response);
+    this.setState({ movies: response.data });
+
+    /*
     const response2 = await axios.get(baseURL2);
     console.log(response2.data);
     //this.setState({movies:response2.data});
     this.setState({movies:response2.data.results});
 */
-
   }
 
   /* 
@@ -89,7 +89,6 @@ class App extends React.Component {
   };
 
   render() {
-  
     let filteredMovies = this.state.movies.filter((movie) => {
       return (
         movie.name
@@ -97,23 +96,34 @@ class App extends React.Component {
           .indexOf(this.state.searchQuery.toLowerCase()) !== -1
       );
     });
-  
-  
+
     return (
-      <div>
+      <Router>
         <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <SearchBar searchMovieProp={this.searchMovie}></SearchBar>
-            </div>
-          </div>
-          <MovieList
-            movies={filteredMovies}
-            deleteMovieProp={this.deleteMovie}
-          ></MovieList>
-          <AddMovie></AddMovie>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={() => (
+                <React.Fragment>
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <SearchBar searchMovieProp={this.searchMovie}></SearchBar>
+                    </div>
+                  </div>
+
+                  <MovieList
+                    movies={filteredMovies}
+                    deleteMovieProp={this.deleteMovie}
+                  ></MovieList>
+                </React.Fragment>
+              )}
+            ></Route>
+
+            <Route path="/add-movie" exact component={AddMovie} />
+          </Switch>
         </div>
-      </div>
+      </Router>
     );
   }
 }
