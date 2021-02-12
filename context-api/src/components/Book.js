d356acc9efa1e70f8d26eb34ceb483927a717c7d
Ahmet Suhan Oka <ahmetsuhanoka0@gmail.com>
2021-02-12 20:51:10 +0300

    Css codes added for both Book and BookList components

--- context-api/src/components/Book.js
+++ context-api/src/components/Book.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
-
+import './Book.css';
 export class Book extends Component {
     render() {
         //console.log(this.props.book);
         return (
-            <div>
-                <h2>{this.props.book.title}</h2>
-                <h4>{this.props.book.author}</h4>
-              <img src = {this.props.book.imageURL}></img>
-            </div>
+           <div className="col-lg-4 col-sm-6 mb-4">
+               <div className="portfolio-item">
+                    <a className="portfolio-link" dataToggle="modal" href="#portfolioModal1">
+                        <div className="portfolio-hover" >
+                            <div className="portfolio-hover-content"><i className="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img className="img-fluid" src={this.props.book.imageURL} alt=""></img>
+                    </a>
+                    <div className="portfolio-caption">
+                        <div className="portfolio-caption-heading">{this.props.book.title}</div>
+                        <div className="portfolio-caption-sunheading text-muted" >{this.props.book.author}</div>
+                    </div>
+               </div>
+           </div>
         )
     }
 }
--- context-api/src/components/BookList.js
+++ context-api/src/components/BookList.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react'
-import Book from './Book';
- class BookList extends Component {
+import React, { Component } from "react";
+import Book from "./Book";
+import './BookList.css';
+class BookList extends Component {
+  render() {
+    //console.log(this.props);
 
-    render() {
+    const bookList = this.props.books.map((book, index) => {
+      return <Book book={book} key={index}></Book>;
+    });
 
-        //console.log(this.props);
-
-        const bookList = this.props.books.map((book,index) => {
-            return <Book book={book} key={index}></Book>
-        } );
-        
-        return (
-            <div>
-                {bookList}
-            </div>
-        )
-    }
+    return (
+      <section className="page-section bg-light" id="portfolio">
+        <div className="container">
+          <div className="text-center">
+            <h2 className="section-heading text-uppercase">Bookfolio</h2>
+            <h3 className="section-subheading text-muted">lorem ipsum dolor</h3>
+          </div>
+          <div className="row">{bookList}</div>
+        </div>
+      </section>
+    );
+  }
 }
 export default BookList;
