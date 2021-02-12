15108f5ba2f3567ad8d8a0e4f8f42ba14070b12f
Ahmet Suhan Oka <ahmetsuhanoka0@gmail.com>
2021-02-12 21:19:03 +0300

    parent to child data transfer operations ie Props drilling turned into context api default value

--- context-api/src/components/BookList.js
+++ context-api/src/components/BookList.js
@@ -1,25 +1,33 @@
 import React, { Component } from "react";
 import Book from "./Book";
 import './BookList.css';
+import BookContext from '../contexts/BookContext';
+
 class BookList extends Component {
+  static contextType=BookContext;
+  
   render() {
     //console.log(this.props);
 
-    const bookList = this.props.books.map((book, index) => {
-      return <Book book={book} key={index}></Book>;
-    });
+    console.log(this.context);
+    const books=this.context;
+  
 
     return (
       <section className="page-section bg-light" id="portfolio">
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">Bookfolio</h2>
-            <h3 className="section-subheading text-muted">lorem ipsum dolor</h3>
-          </div>
-          <div className="row">{bookList}</div>
+      <div className="container">
+        <div className="text-center">
+          <h2 className="section-heading text-uppercase">Bookfolio</h2>
+          <h3 className="section-subheading text-muted">lorem ipsum dolor</h3>
         </div>
-      </section>
-    );
+        <div className="row">{books.map((book,index) => {
+          return <Book book={book}
+                        key ={index}
+          ></Book>
+        })}</div>
+      </div>
+    </section>
+    )
   }
 }
 export default BookList;
